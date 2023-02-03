5004105448718af5614eaa76ea75620bcc87e7a3
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-02-03 21:22:05 +0000

    Correct public/css filename; add static handler to server.js as middleware to serve public directory files; add sample css to test

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const { homeHandler } = require("./routes");
 
 const server = express();
 
+const staticHandler = express.static("public");
+
+server.use(staticHandler);
+
 server.get("/", homeHandler);
 
 module.exports = { server };
