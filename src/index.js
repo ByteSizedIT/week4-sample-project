1d8d31a95398b8fcee91992ec989807252f23b0f
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-02-03 17:33:34 +0000

    Create server and basic home route in server.js; install dotenv for accessing env vars from .env file in root; update index.js to start server; add dev and start scripts to package.json, running index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { server } = require("./server.js");
+
+// const port = process.env.PORT || 3000;
+require("dotenv").config();
+const port = process.env.PORT;
+// console.log(process.env);
+
+server.listen(port, () => console.log(`Listening at http://localhost:${port}`));
