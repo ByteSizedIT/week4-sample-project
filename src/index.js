e56c97ccb253e9757e3bf77829dd7d25f2eb2634
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-02-03 22:33:56 +0000

    Update assigned port in index.js to try and resolve CD issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { server } = require("./server.js");
 
 // const port = process.env.PORT || 3000;
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 // console.log(process.env);
 
 server.listen(port, () => console.log(`Listening at http://localhost:${port}`));
