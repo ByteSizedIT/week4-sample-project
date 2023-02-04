1cddb67b7682d0ae84b468138e1b22378615d5e8
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-02-04 02:13:19 +0000

    Add route for the Authorization Callback URL in gitHub OAuth App settings, used to redirect user back to app (step 2 of Authorizing OAuth Apps); create temp route to redirect the response to for testing

--- src/server.js
+++ src/server.js
@@ -13,4 +13,12 @@ server.use(cookieParser(process.env.COOKIE_SECRET));
 
 server.get("/", homeHandler);
 
+server.get("/oauth2callback", (req, res) => {
+  res.redirect("/hell-yeah");
+});
+
+server.get("/hell-yeah", (req, res) => {
+  res.send(`<h1>Authorized!</h1>`);
+});
+
 module.exports = { server };
