7e0f7ef3a13d537ff34283a0188bafe5fcd4c206
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-02-06 23:11:18 +0000

    Rename file with gitHub Oauth API functions 'api.js' (from oauth.js); Create dir called 'routeHandlers' containing individual files for each route handler function (previously combined in one file called routes.js)

---
+++ src/routeHandlers/oauth.js
@@ -0,0 +1,31 @@
+// Fetch requests to GitHub api stored in oauth.js
+const api = require("../api.js");
+
+// If user accepts auth request, GitHub redirects back to route below..
+// ..with a temp code in a code parameter
+// as well as the state you provided in the previous step in a state parameter.
+// The temporary code will expire after 10 minutes.
+// POST this code to GH to get an access_token for talking to their API
+// If the states don't match, then a third party created the request, and you should abort the process.
+function oauthHandler(req, res) {
+  const code = req.query.code;
+  api
+    .getToken(code)
+    .then(api.getUser)
+    .then((user) => {
+      console.log({ user });
+      // TODO:
+      // create a new user in app DB
+      // create session in app DB and ref in cookies etc
+      // Interim temp solution adds the username into the cookie
+      res.cookie("user", user.login, {
+        httpOnly: true,
+        signed: true,
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite: "lax",
+      });
+      res.redirect("/hell-yeah");
+    });
+}
+
+module.exports = oauthHandler;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const { homeHandler, oauthHandler } = require("./routes");
+const homeHandler = require("./routeHandlers/home");
+const oauthHandler = require("./routeHandlers/oauth");
 
 const server = express();
 
@@ -13,12 +14,7 @@ server.use(cookieParser(process.env.COOKIE_SECRET));
 
 server.get("/", homeHandler);
 
-// If user accepts auth request, GitHub redirects back to route below..
-// ..with a temp code in a code parameter
-// as well as the state you provided in the previous step in a state parameter.
-// The temporary code will expire after 10 minutes.
-// POST this code to GH to get an access_token for talking to their API
-// If the states don't match, then a third party created the request, and you should abort the process.
+// If user accepts auth request, GitHub redirects back to route below
 server.get("/oauth2callback", oauthHandler);
 
 server.get("/hell-yeah", (req, res) => {
